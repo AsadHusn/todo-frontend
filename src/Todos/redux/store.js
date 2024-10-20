81f8ba25befd7f382098be70d5d69a560b70ef92
AxAd <asadhusn007@gmail.com>
2024-10-20 15:39:08 +0500

    redux thunk middleware with extension issue

--- src/Todos/redux/store.js
+++ src/Todos/redux/store.js
@@ -2,10 +2,16 @@ import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
 import { thunk } from "redux-thunk";
 
-export default createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+// NOTE: this will result in error if redux extension is not enable better to use 2nd way as following
+// const store1 = createStore(
+//   reducers,
+//   compose(
+//     applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store2 = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+export default store2;
